594368c6dca24ee928d6696a161d74b1979bdc00
Xiangpeng Hao <3150102255@zju.edu.cn>
2019-03-05 15:22:36 -0800

    update test code

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 from benchmark import SpectralImage, BaseBench
 from algorithms.shades_of_gray import AverageRGB, MaxRGB, PNorm
 
+test_file = 'test1.exr'
 
-algs: [BaseBench] = [PNorm]
-img = SpectralImage.NewFromFile('fixtures/test0.exr')
+algs: [BaseBench] = [AverageRGB, MaxRGB, PNorm]
+img = SpectralImage.NewFromFile(f'fixtures/{test_file}')
 
 for algorithm in algs:
     refl = algorithm(img, None).get_test_reflectance()
-    refl.dump_to_sRGB_image(f'dist/{algorithm.NAME}_refl.exr')
+    refl.dump_to_sRGB_image(f'dist/{test_file}_{algorithm.NAME}_refl.exr')
