bd4627d2e063bed73019a314eedc75a11292c590
Xiangpeng Hao <xha62@sfu.ca>
2019-03-10 22:23:05 -0700

    adapt shades of gray

--- algorithms/shades_of_gray.py
+++ algorithms/shades_of_gray.py
@@ -1,5 +1,6 @@
-from benchmark import SpectralImage, BaseBench
+from benchmark import BaseBench
 import numpy as np
+from images import sRGBImage, SpectralImage
 
 SPECTRUM_LENGTH = 61
 
@@ -7,35 +8,35 @@ SPECTRUM_LENGTH = 61
 class AverageRGB(BaseBench):
     NAME = 'average_rgb'
 
-    def get_test_reflectance(self) -> SpectralImage:
-        estimated = np.ndarray(SPECTRUM_LENGTH)
-        for i in range(SPECTRUM_LENGTH):
-            estimated[i] = np.average(self.test_img.spectrum[:, :, i])
+    def get_test_reflectance(self) -> sRGBImage:
+        estimated = np.zeros(3)
+        for i in range(3):
+            estimated[i] = np.average(self.test_img.img_data[:, :, i])
 
         shape = self.test_img.img_shape
-        reflectance = np.zeros((*shape, SPECTRUM_LENGTH))
+        reflectance = np.zeros((*shape, 3))
         for i in range(shape[0]):
             for j in range(shape[1]):
-                reflectance[i, j, :] = self.test_img.spectrum[i,
+                reflectance[i, j, :] = self.test_img.img_data[i,
                                                               j, :]/estimated
-        return SpectralImage.NewFromSpectrum(reflectance)
+        return sRGBImage.NewFromArray(reflectance)
 
 
 class MaxRGB(BaseBench):
     NAME = 'max_rgb'
 
-    def get_test_reflectance(self) -> SpectralImage:
-        estimated = np.ndarray(SPECTRUM_LENGTH)
-        for i in range(SPECTRUM_LENGTH):
-            estimated[i] = np.max(self.test_img.spectrum[:, :, i])
+    def get_test_reflectance(self) -> sRGBImage:
+        estimated = np.zeros(3)
+        for i in range(3):
+            estimated[i] = np.max(self.test_img.img_data[:, :, i])
 
         shape = self.test_img.img_shape
-        reflectance = np.zeros((*shape, SPECTRUM_LENGTH))
+        reflectance = np.zeros((*shape, 3))
         for i in range(shape[0]):
             for j in range(shape[1]):
-                reflectance[i, j, :] = self.test_img.spectrum[i,
+                reflectance[i, j, :] = self.test_img.img_data[i,
                                                               j, :]/estimated
-        return SpectralImage.NewFromSpectrum(reflectance)
+        return sRGBImage.NewFromArray(reflectance)
 
 
 class PNorm(BaseBench):
@@ -43,23 +44,23 @@ class PNorm(BaseBench):
 
     P = 5
 
-    def get_test_reflectance(self) -> SpectralImage:
-        estimated = np.ndarray(SPECTRUM_LENGTH)
+    def get_test_reflectance(self) -> sRGBImage:
+        estimated = np.zeros(3)
 
         img_shape = self.test_img.img_shape
 
         N = img_shape[0]*img_shape[1]
         N_1_p = np.power(N, 1/self.P)
-        for i in range(SPECTRUM_LENGTH):
-            wavelength = self.test_img.spectrum[:, :, i]
-            wavelength.shape = (img_shape[0]*img_shape[1],)
-            wavelength = wavelength.astype(np.float64)
-            estimated[i] = np.linalg.norm(wavelength, self.P)/N_1_p
+        for i in range(3):
+            rgb_value = self.test_img.img_data[:, :, i]
+            rgb_value.shape = (img_shape[0]*img_shape[1],)
+            rgb_value = rgb_value.astype(np.float64)
+            estimated[i] = np.linalg.norm(rgb_value, self.P)/N_1_p
 
         shape = self.test_img.img_shape
-        reflectance = np.zeros((*shape, SPECTRUM_LENGTH))
+        reflectance = np.zeros((*shape, 3))
         for i in range(shape[0]):
             for j in range(shape[1]):
-                reflectance[i, j, :] = self.test_img.spectrum[i,
+                reflectance[i, j, :] = self.test_img.img_data[i,
                                                               j, :]/estimated
-        return SpectralImage.NewFromSpectrum(reflectance)
+        return sRGBImage.NewFromArray(reflectance)
--- benchmark.py
+++ benchmark.py
@@ -1,86 +1,22 @@
 import OpenEXR
 import Imath
-from typing import Dict
 import numpy as np
 from color_space import Spectrum, RGB, XYZ
-import os
-from observers import BaseObserver, two_degree_observer
-
-
-float_pixel = Imath.PixelType(Imath.PixelType.FLOAT)
-half_pixel = Imath.PixelType(Imath.PixelType.HALF)
-channels: [int] = list(range(400, 700, 5))
-
-
-class SpectralImage:
-    def __init__(self, img_file: str = None, spectrum: np.ndarray = None):
-        self.img_file = img_file
-        self.img_shape = spectrum.shape[:-1]
-        self.spectrum = spectrum
-
-    @classmethod
-    def NewFromSpectrum(cls, spectrum: np.ndarray):
-        return SpectralImage(None, spectrum)
-
-    @classmethod
-    def NewFromFile(cls, file_path: str):
-        img_file = OpenEXR.InputFile(file_path)
-        dw = img_file.header()['dataWindow']
-        img_shape = (dw.max.y - dw.min.y + 1, dw.max.x - dw.min.x + 1)
-
-        wave_length_map: Dict[int, np.ndarray] = {}
-        for c in channels:
-            buffed = img_file.channel(str(c), half_pixel)
-            channel_data = np.frombuffer(buffed, dtype=np.float16)
-            wave_length_map[c] = channel_data.reshape((*img_shape, 1))
-
-        # in case the wave length in the headers are not sorted
-        sorted_waves = sorted(wave_length_map.keys())
-        spectrum = wave_length_map[sorted_waves[0]]
-        for key in sorted_waves[1:]:
-            spectrum = np.append(spectrum, wave_length_map[key], axis=2)
-        # pad to 400, 700 inclusive
-        spectrum = np.append(
-            spectrum, wave_length_map[sorted_waves[-1]], axis=2)
-        return SpectralImage(img_file, spectrum)
-
-    def dump_to_sRGB_image(self, output: str, observer: BaseObserver = two_degree_observer):
-        rgb_image = np.zeros(
-            (self.img_shape[0], self.img_shape[1], 3), dtype=np.double)
-        for i in range(self.img_shape[0]):
-            for j in range(self.img_shape[1]):
-                rgb_image[i, j, :] = observer.spec_to_srgb(
-                    self.spectrum[i, j, :]).np_rgb
-
-        header = OpenEXR.Header(self.img_shape[1], self.img_shape[0])
-        half_chan = Imath.Channel(half_pixel)
-        header['channels'] = dict([(c, half_chan) for c in 'RGB'])
-        exr = OpenEXR.OutputFile(output, header)
-        exr.writePixels({'R': rgb_image[:, :, 0].astype(np.float16).tostring(),
-                         'G': rgb_image[:, :, 1].astype(np.float16).tostring(),
-                         'B': rgb_image[:, :, 2].astype(np.float16).tostring()})
-        exr.close()
-
-    @property
-    def header(self):
-        return self.img_file.header()
-
-    def __getitem__(self, wavelength: int):
-        return self.spectrum[:, :, (wavelength-400)//5]
+from images import sRGBImage, SpectralImage
 
 
 class BaseBench:
 
     NAME: str = ""
 
-    def __init__(self, test_img: SpectralImage, truth_img: SpectralImage):
+    def __init__(self, test_img: sRGBImage, truth_img: sRGBImage):
         self.test_img = test_img
         self.ground_truth = truth_img
 
-    def get_test_reflectance(self) -> SpectralImage:
+    def get_test_reflectance(self) -> sRGBImage:
         raise NotImplementedError("function should be overrided")
 
-    def get_test_emission_map(self) -> SpectralImage:
+    def get_test_emission_map(self) -> sRGBImage:
         raise NotImplementedError("function should be overrided")
 
     # probably we should have a evaluate
