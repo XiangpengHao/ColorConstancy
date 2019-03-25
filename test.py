e5bf9bc98bbcc0c5b1fb338c7d9319d90f4129aa
Xiangpeng Hao <xha62@sfu.ca>
2019-03-24 17:28:23 -0700

    add test file

--- test.py
+++ test.py
@@ -15,7 +15,11 @@ for algorithm in algs:
     refl = alg_ins.get_reflectance()
     refl.dump_file(f'dist/{test_file}_{algorithm.NAME}_refl.exr')
     angular_error = alg_ins.get_error(groundtruth_img, get_angle)
-    distance_error = alg_ins.get_error(
-        groundtruth_img, get_chrom_distance)
     alg_ins.draw_heatmap(angular_error, f'{test_file}_ang')
-    alg_ins.draw_heatmap(distance_error, f'{test_file}_dis')
+
+base_bench = BaseBench(test_img)
+light = base_bench.get_best_single_illuminant(groundtruth_img)
+best_single = base_bench.adjust_single_illuminant(light)
+best_single.dump_file(f'dist/{test_file}_best_refl.exr')
+angular_error = base_bench.get_error(groundtruth_img, get_angle)
+base_bench.draw_heatmap(angular_error, f'{test_file}_best_ang')
