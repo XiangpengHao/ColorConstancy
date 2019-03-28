a74424d24975eabee5bbe91c0aa87debf8b1bcf8
Xiangpeng Hao <xha62@sfu.ca>
2019-03-27 20:00:05 -0700

    adapt new structure

--- benchmark.py
+++ benchmark.py
@@ -6,12 +6,13 @@ from images import RGBImage, SpectralImage
 import matplotlib
 import matplotlib.pyplot as plt
 import os
+import functools
 
 import logging
 logging.getLogger('matplotlib').setLevel(logging.WARNING)
 
 
-def get_angle(v1, v2):
+def metric_angle(v1, v2):
     norm_v1 = np.linalg.norm(v1)
     norm_v2 = np.linalg.norm(v2)
     if(norm_v1 == 0 or norm_v2 == 0):
@@ -20,7 +21,7 @@ def get_angle(v1, v2):
                                 (np.linalg.norm(v1)*np.linalg.norm(v2))))
 
 
-def get_chrom_distance(v1, v2):
+def metric_chrom_distance(v1, v2):
     # this is by no means reasonable, but it happens
     if sum(v1) == 0 or sum(v2) == 0:
         return 0
@@ -33,16 +34,17 @@ class BaseBench:
 
     NAME: str = ""
 
-    reflectance_map: RGBImage = None
     angular_error: np.ndarray = None
 
-    def __init__(self, input_dir: str, output_dir: str):
+    def __init__(self, input_dir: str, output_dir: str, groundtruth_dir: str):
         self.input_dir = input_dir
         self.output_dir = output_dir
+        self.groundtruth_dir = groundtruth_dir
+
         self.img_list = os.listdir(input_dir)
 
-        self.curr_idx = 0
-        self.curr_img = None
+        self.curr_idx: int = 0
+        self.curr_refl: RGBImage = None
 
     def get_emission(self) -> RGBImage:
         raise NotImplementedError("function should be overrided")
@@ -50,19 +52,23 @@ class BaseBench:
     def run(self):
         raise NotImplementedError("function should be overrided")
 
+    @property
+    def curr_img_name(self):
+        return self.img_list[self.curr_idx]
+
     def has_next(self):
         return self.curr_idx < len(self.img_list)
 
     def get_next_refl(self) -> RGBImage:
         if not self.has_next():
             return None
-        self.curr_img = RGBImage.NewFromFile(
+        self.curr_refl = RGBImage.NewFromFile(
             f'{self.output_dir}/{self.img_list[self.curr_idx]}')
-        return self.curr_img
+        return self.curr_refl
 
     def get_best_single_adjustment(self, ground_truth: RGBImage) -> np.array:
-        shape = self.curr_img.img_shape
-        xyz_img = self.curr_img.get_xyz_image()
+        shape = self.curr_refl.img_shape
+        xyz_img = self.curr_refl.get_xyz_image()
         xyz_truth = ground_truth.get_xyz_image()
 
         rv = []
@@ -74,37 +80,42 @@ class BaseBench:
         adjusted_img = np.zeros((*shape, 3))
         for i in range(shape[0]):
             for j in range(shape[1]):
-                raw_xyz = RGB(*self.curr_img[i, j, :], True).to_xyz()
+                raw_xyz = RGB(*self.curr_refl[i, j, :], True).to_xyz()
                 adjusted_xyz = np.multiply(xyz_img[i, j, :], rv) * \
                     (raw_xyz.y/xyz_img[i, j, 1])
                 adjusted_img[i, j, :] = XYZ(*adjusted_xyz).to_rgb().np_rgb
 
-        self.reflectance_map = RGBImage.NewFromArray(adjusted_img)
-        return self.reflectance_map
+        self.curr_refl= RGBImage.NewFromArray(adjusted_img)
+        return self.curr_refl
 
     def adjust_single_illuminant(self, illuminant: np.array) -> RGBImage:
-        shape = self.curr_img.img_shape
+        shape = self.curr_refl.img_shape
 
         reflectance = np.zeros((*shape, 3))
         for i in range(shape[0]):
             for j in range(shape[1]):
-                reflectance[i, j, :] = self.curr_img.img_data[i,
-                                                              j, :]/illuminant
-        self.reflectance_map = RGBImage.NewFromArray(reflectance)
-        return self.reflectance_map
+                reflectance[i, j, :] = self.curr_refl.img_data[i,
+                                                               j, :]/illuminant
+        self.curr_refl= RGBImage.NewFromArray(reflectance)
+        return self.curr_refl
 
-    def get_error(self, ground_truth: RGBImage, metric=get_angle) -> np.ndarray:
-        if(self.reflectance_map == None):
-            self.reflectance_map = self.get_reflectance()
-        img_shape = self.reflectance_map.img_shape
+    def get_error(self, metric=metric_angle) -> np.ndarray:
+        if(self.curr_refl == None):
+            return None
+        ground_truth = self.get_groundtruth(self.curr_img_name)
+        img_shape = self.curr_refl.img_shape
         result = np.zeros(img_shape)
         for i in range(img_shape[0]):
             for j in range(img_shape[1]):
                 result[i, j] = metric(
-                    self.reflectance_map[i, j], ground_truth[i, j])
+                    self.curr_refl[i, j], ground_truth[i, j])
 
-        self.angular_error = result
         return result
+
+    @functools.lru_cache(maxsize=30)
+    def get_groundtruth(self, curr_img_name: str):
+        curr_img_prefix = curr_img_name.split('.')[0]
+        return RGBImage.NewFromFile(f'{self.groundtruth_dir}/{curr_img_prefix}_truth.exr')
 
     def draw_heatmap(self, error_map, output_prefix: str):
         plt.figure()
--- test.py
+++ test.py
@@ -1,33 +1,21 @@
-from benchmark import BaseBench, get_angle, get_chrom_distance
+from benchmark import BaseBench, metric_angle, metric_chrom_distance
 from algorithms.shades_of_gray import AverageRGB, MaxRGB, PNorm
 from algorithms.mixed_illuminant_kmeans import MixedKmeans
 from images import SpectralImage, RGBImage
 
-test_file = 'test1.exr'
-groundtruth_file = 'test1_truth.exr'
 
 test_dir = 'fixtures/test'
-groundtruth_dir = 'fixtures/test'
+output_dir = 'dist'
+groundtruth_dir = 'fixtures/ground_truth'
 
 algs: [BaseBench] = [AverageRGB, MaxRGB]
-test_img = RGBImage.NewFromFile(f'fixtures/{test_file}')
-
-groundtruth_img = RGBImage.NewFromFile(f'fixtures/{groundtruth_file}')
 
 for algorithm in algs:
-    alg_ins = algorithm(test_img).run()
-
-    refl = alg_ins.get_reflectance()
-    refl.dump_file(f'dist/{test_file}_{algorithm.NAME}_refl.exr')
-    angular_error = alg_ins.get_error(groundtruth_img, get_angle)
-    distance_error = alg_ins.get_error(groundtruth_img, get_chrom_distance)
-    alg_ins.draw_heatmap(angular_error, f'{test_file}_ang')
-    alg_ins.draw_heatmap(distance_error, f'{test_file}_dis')
-
-base_bench = BaseBench(test_img)
-best_single = base_bench.get_best_single_adjustment(groundtruth_img)
-best_single.dump_file(f'dist/{test_file}_best_refl.exr')
-angular_error = base_bench.get_error(groundtruth_img, get_angle)
-dis_error = base_bench.get_error(groundtruth_img, get_chrom_distance)
-base_bench.draw_heatmap(dis_error, f'{test_file}_best_dis')
-base_bench.draw_heatmap(angular_error, f'{test_file}_best_ang')
+    alg_ins = algorithm(test_dir, output_dir, groundtruth_dir)
+    alg_ins.run()
+    while(alg_ins.has_next()):
+        refl = alg_ins.get_next_refl()
+        angular_err = alg_ins.get_error(metric_angle)
+        distance_err = alg_ins.get_error(metric_chrom_distance)
+        alg_ins.draw_heatmap(angular_err, f'{alg_ins.curr_img_name}_ang')
+        alg_ins.draw_heatmap(distance_err, f'{alg_ins.curr_img_name}_dis')
